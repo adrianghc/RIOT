575c474f5a7266561bcfda71c6f45c2b6bf7385e
Adrian GHC <adrian.ghc@outlook.com>
2017-09-19 22:38:06 +0200

    tests/unittests/tests-keccak: Update with console print-outs and fix bugs

--- tests/unittests/tests-keccak/tests-keccak.c
+++ tests/unittests/tests-keccak/tests-keccak.c
@@ -19,6 +19,8 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 
 #include "KeccakHash800.h"
@@ -29,42 +31,61 @@
 #define KeccakP400_excluded
 #define KeccakP1600_excluded
 
-Keccak_HashInstance hashInstance;
+Keccak_HashInstance* hashInstance;
 
 const unsigned int rate = 480;
-const unsigned int capacity = 320;
 const unsigned int hashbitlen = 256;
-const unsigned char delimitedSuffix = '\0';
+const unsigned char delimitedSuffix = '\1';
 
-const BitLength databitlen = 10;
-const char datastring[10] = "0123456789";
+const BitLength databitlen = 80;
+const char datastring[] = "0123456789";
 const BitSequence* data = (unsigned char*) datastring;
 
-const char hashvalstring[32] = "";
-BitSequence hashval[32];
+const char hashvalstring[] = "792fd532040b69e7681598742ebc209f9ec1871a290627fbf72cad13346819df";
+BitSequence* hashval;
 
 static void setUp(void)
 {
     /* Initialize */
+    hashInstance = malloc(sizeof(Keccak_HashInstance));
+    hashval = malloc(hashbitlen / 8);
 }
 
 static void tearDown(void)
 {
     /* Finalize */
+    free(hashInstance);
+    free(hashval);
 }
 
 static void test_keccak(void)
 {
-    HashReturn return_init = Keccak_HashInitialize(&hashInstance, rate, capacity, hashbitlen, delimitedSuffix);
+    printf("\nTesting hash initialization");
+    HashReturn return_init = Keccak_HashInitialize(hashInstance, rate, 800-rate, hashbitlen, delimitedSuffix);
     TEST_ASSERT_EQUAL_INT(0, return_init);
 
-    HashReturn return_update = Keccak_HashUpdate(&hashInstance, data, databitlen);
+    printf("\nTesting hash update");
+    HashReturn return_update = Keccak_HashUpdate(hashInstance, data, databitlen);
     TEST_ASSERT_EQUAL_INT(0, return_update);
 
-    HashReturn return_final = Keccak_HashFinal(&hashInstance, hashval);
+    printf("\nTesting hash finalization");
+    HashReturn return_final = Keccak_HashFinal(hashInstance, hashval);
     TEST_ASSERT_EQUAL_INT(0, return_final);
 
-    TEST_ASSERT_EQUAL_STRING((char*) hashvalstring, (char*) hashval);
+    printf("\nComparing digest with expected result");
+    uint8_t eq = 1;
+    char hashvalbuf[2];
+
+    for (uint8_t i=0; i<hashbitlen/8; i++) {
+        sprintf(hashvalbuf, "%02x", hashval[i]);
+        
+        if (hashvalbuf[0] != hashvalstring[2*i] || hashvalbuf[1] != hashvalstring[2*i+1]) {
+            eq = 0;
+            break;
+        }
+    }
+
+    TEST_ASSERT_EQUAL_INT(1, eq);
 }
 
 Test *tests_keccak_all(void)
